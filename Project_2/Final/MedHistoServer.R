17edeae028ffa3de0186da2d5ce796326dd95572
Jeff Feng <feng.jeffrey@utexas.edu>
2018-02-27 19:36:17 -0600

    fixhistogram

--- Project_2/Final/MedHistoServer.R
+++ Project_2/Final/MedHistoServer.R
@@ -1,40 +1,40 @@
-# Change all __1_s, Download information, and edit the plot and ggplotly arguments below:
+# Change all __4_s, Download information, and edit the plot and ggplotly arguments below:
 # Build Download Button and Output DataTable---------------
-output$download__1_ <- downloadHandler(
-  filename = function(){"Histogram.csv"}, 
+output$download__4_ <- downloadHandler(
+  filename = function(){"Hospital_Visit_Data_TX.csv"}, 
   content = function(fname){
-    write.csv(dfA1(), fname)
+    write.csv(df5(), fname)
   }
 )
-output$table__1_ <- renderDataTable({
-  DT::datatable(dfA1() , rownames = FALSE, extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+output$table__4_ <- renderDataTable({
+  DT::datatable(df5() , rownames = FALSE, extensions = list(Responsive = TRUE, FixedHeader = TRUE)
   )
 })
 # ---------------------------------------------------------
 # Build and Output Throughput Plot ------------------------
-output$Histogram__1_ <- renderPlotly({
-  plot = dfA1() %>% ggplot() + 
-    geom_histogram(mapping = aes(x=shipping_cost, colour = region, fill = region), binwidth = 10) +
+output$Histogram__4_ <- renderPlotly({
+  plot = df5() %>% ggplot() + 
+    geom_histogram(mapping = aes(x=average_medicare_payments, colour = drg_definition, fill = drg_definition), binwidth = 1500) +
     
     theme_bw() +
-    theme(plot.title = element_text(size = input$titleFont__1_, face = "bold")) + 
+    theme(plot.title = element_text(size = input$titleFont__4_, face = "bold")) + 
     theme( # Legend Attributes - see https://github.com/tidyverse/ggplot2/wiki/Legend-Attributes
-      legend.title=element_text(size=input$legendTitleSize__1_), 
-      legend.text=element_text(size=input$legendItemSize__1_),
-      legend.key = element_rect(size = input$legendKeySize__1_),
-      legend.key.size = unit(input$legendKeySize__1_, 'lines')) +
-    theme(axis.text=element_text(size=input$textFont__1_),
-          axis.title=element_text(size=input$textFont__1_, face="bold"),
+      legend.title=element_text(size=input$legendTitleSize__4_), 
+      legend.text=element_text(size=input$legendItemSize__4_),
+      legend.key = element_rect(size = input$legendKeySize__4_),
+      legend.key.size = unit(input$legendKeySize__4_, 'lines')) +
+    theme(axis.text=element_text(size=input$textFont__4_),
+          axis.title=element_text(size=input$textFont__4_, face="bold"),
           axis.text.x = element_text(angle = 45, hjust = 1))  +
     theme(plot.margin=unit(c(2,2,2,2),"cm")) +
     scale_y_continuous(labels = scales::comma) + # Disable scientific notation
-    ggtitle(paste(Regions(), input$title__1_)) +
-    xlab(input$xLabel__1_) + ylab(input$yLabel__1_) +
+    ggtitle(paste(input$title__4_)) +
+    xlab(input$xLabel__4_) + ylab(input$yLabel__4_) +
     theme(axis.title.y = element_text(margin = margin(t = 10, r = 10, b = 10, l = 10))) +
-    scale_color_discrete(name = input$legendTitle__1_)
+    scale_color_discrete(name = input$legendTitle__4_)
   
-  if( ! is.na(input$plotWidth__1_) & ! is.na(input$plotHeight__1_))
-    ggplotly(plot, tooltip = c("shipping_cost"), session="knitr", width = input$plotWidth__1_, height = input$plotHeight__1_)
+  if( ! is.na(input$plotWidth__4_) & ! is.na(input$plotHeight__4_))
+    ggplotly(plot, tooltip = c("average"), session="knitr", width = input$plotWidth__4_, height = input$plotHeight__4_)
   else
     ggplotly(plot, tooltip = c("shipping_cost"), session="knitr") 
 })
