6ce48166dcfeff4c4a21dc35686f5fb9ec1b3fe3
Jeff Feng <feng.jeffrey@utexas.edu>
2018-03-19 20:55:28 -0500

    jeffspart

---
+++ Project_3/jeffspart/DataB.R
@@ -0,0 +1,23 @@
+dfB <- eventReactive(c(input$selectCountry_1,input$ExpFilter), { 
+  project <- "https://data.world/jeffrey-feng/s18-edv-project-3" 
+  data.world::set_config(cfg_env("DW_API")) 
+  paramQuery <- data.world::qry_sql(
+    "   
+    select country,healthy_life_expectancy_hale_at_birth_years_both_sexes,total_expenditure_on_health_as_percentage_of_gross_domestic_product
+    from WHO_Cleaner 
+    where country in (?, ?, ?, ?,?,?,?,?,?,?) and total_expenditure_on_health_as_percentage_of_gross_domestic_product between ? and ? 
+    order by healthy_life_expectancy_hale_at_birth_years_both_sexes
+    
+    ")
+  paramQuery$params <- c(input$selectCountry_1[1], input$selectCountry_1[2], input$selectCountry_1[3], input$selectCountry_1[4], input$selectCountry_1[5], input$selectCountry_1[6], input$selectCountry_1[7], input$selectCountry_1[8], input$selectCountry_1[9], input$selectCountry_1[10],input$ExpFilter[1],input$ExpFilter[2])
+  data.world::query(paramQuery, dataset = project)
+})
+
+dfB1 <- eventReactive(c(input$ExpFilter,input$selectCountry_1, input$yDataMin__1_, input$yDataMax__1_), { 
+  if( ! is.na(input$yDataMin__1_) & ! is.na(input$yDataMax__1_)) {
+    dfB() %>% dplyr::filter(between(healthy_life_expectancy_hale_at_birth_years_both_sexes, input$yDataMin__1_, input$yDataMax__1_))
+  }
+  else {
+    dfB()
+  }
+})
